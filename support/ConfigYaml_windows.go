25409b7a927a40d902631d9d81e72725e18e1dd1
donny rolanda <donny.rolanda@gmail.com>
2024-03-29 11:15:45 +0800

    # Ondevelop - Working improve delete current app on windows

--- support/ConfigYaml_windows.go
+++ support/ConfigYaml_windows.go
@@ -8,7 +8,6 @@ import (
 )
 
 func ReplaceApp(executablePath string, app_new string) *exec.Cmd {
-	cmd := exec.Command("cmd", "del "+executablePath+" && move "+app_new+" "+executablePath)
-	cmd.CombinedOutput()
+	cmd := exec.Command("cmd", "ping google.co.id && del "+executablePath+" && move "+app_new+" "+executablePath)
 	return cmd
 }
